73ee552a819e63ca036f4d992a02429e56842851
AsenLins <736239001@qq.com>
2018-06-28 00:20:42 +0800

    update code

--- src/stories/index.stories.js
+++ src/stories/index.stories.js
@@ -3,10 +3,13 @@ import { storiesOf } from '@storybook/vue';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import vantUIForm from '../components/form/vantUIForm.vue';
+import vantUIForm from '../components/form/vant/form.vue';
 import { templateFactory } from "../components/form/model/template/templateFactory"
 console.log("templateFactory", templateFactory)
 console.log("testForm", templateFactory["testForm"]());
+
+
+
 
 storiesOf("表单设计器", module).add("表单容器Vant", () => ({
     components: { vantUIForm },
